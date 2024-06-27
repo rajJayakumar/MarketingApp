bd3a18392c1225d825553e2c25cc071cc14b7978
rajJayakumar <157939956+rajJayakumar@users.noreply.github.com>
2024-06-27 12:29:39 -0400

    Initial Commit

---
+++ marketingApp/marketingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketingAppApp.swift
+//  marketingApp
+//
+//  Created by Rajaselvam Jayakumar on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct marketingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
